b036b0eee7c63cacd04bf16cbeb629d6fbdc95ee
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2019-03-04 11:35:08 -0700

    Removing unused code

--- main.go
+++ main.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	// "log"
 	"net/http"
-	"os"
 	"strconv"
 
-	jwtmiddleware "github.com/auth0/go-jwt-middleware"
-	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
-
-type Response struct {
-	Message string `json:"message"`
-}
 
 type Joke struct {
 	ID    int    `json:"id" binding:"required"`
@@ -35,53 +24,7 @@ var jokes = []Joke{
 	Joke{7, 0, "How does a penguin build its house? Igloos it together."},
 }
 
-var jwtMiddleWare *jwtmiddleware.JWTMiddleware
-
-type Jwks struct {
-	Keys []JSONWebKeys `json:"keys"`
-}
-
-type JSONWebKeys struct {
-	Kty string   `json:"kty"`
-	Kid string   `json:"kid"`
-	Use string   `json:"use"`
-	N   string   `json:"n"`
-	E   string   `json:"e"`
-	X5c []string `json:"x5c"`
-}
-
 func main() {
-
-	// Holding off on 0auth for now
-	// jwtMiddleWare := jwtmiddleware.New(jwtmiddleware.Options{
-	// 	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-	// 		aud := os.Getenv("AUTH0_API_AUDIENCE")
-	// 		checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
-	// 		if !checkAudience {
-	// 			return token, errors.New("Invalid audience.")
-	// 		}
-	// 		// verify iss claim
-	// 		iss := os.Getenv("AUTH0_DOMAIN")
-	// 		checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
-	// 		if !checkIss {
-	// 			return token, errors.New("Invalid issuer.")
-	// 		}
-
-	// 		cert, err := getPemCert(token)
-	// 		if err != nil {
-	// 			log.Fatalf("could not get cert: %+v", err)
-	// 		}
-
-	// 		result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
-	// 		return result, nil
-	// 	},
-	// 	SigningMethod: jwt.SigningMethodRS256,
-	// })
-
-	// // register our actual jwtMiddleware
-	// jwtMiddleWare = jwtMiddleWare
-
-	// Set the router as the default one shipped with Gin
 	router := gin.Default()
 
 	router.Use(static.Serve("/", static.LocalFile("./views", true)))
@@ -98,53 +41,7 @@ func main() {
 	api.GET("/jokes", JokeHandler)
 	api.POST("/jokes/like/:jokeID", LikeJoke)
 
-	// Add when integrating 0auth
-	// api.GET("/jokes", authMiddleware(), JokeHandler)
-	// api.POST("/jokes/like/:jokeID", authMiddleware(), LikeJoke)
-
 	router.Run(":3000")
-}
-
-func authMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		err := jwtMiddleWare.CheckJWT(c.Writer, c.Request)
-		if err != nil {
-			fmt.Println(err)
-			c.Abort()
-			c.Writer.WriteHeader(http.StatusUnauthorized)
-			c.Writer.Write([]byte("Unauthorized"))
-			return
-		}
-	}
-}
-
-func getPemCert(token *jwt.Token) (string, error) {
-	cert := ""
-	resp, err := http.Get(os.Getenv("AUTH0_DOMAIN") + ".well-known/jwks.json")
-	if err != nil {
-		return cert, err
-	}
-	defer resp.Body.Close()
-
-	var jwks = Jwks{}
-	err = json.NewDecoder(resp.Body).Decode(&jwks)
-
-	if err != nil {
-		return cert, err
-	}
-
-	x5c := jwks.Keys[0].X5c
-	for k, v := range x5c {
-		if token.Header["kid"] == jwks.Keys[k].Kid {
-			cert = "-----BEGIN CERTIFICATE-----\n" + v + "\n-----END CERTIFICATE-----"
-		}
-	}
-
-	if cert == "" {
-		return cert, errors.New("undable to find appropriate key.")
-	}
-
-	return cert, nil
 }
 
 func JokeHandler(c *gin.Context) {
--- views/js/app.jsx
+++ views/js/app.jsx
@@ -1,112 +1,10 @@
-const AUTH0_CLIENT_ID = "p6owf7Zx1RmyPX1FvAXDZ1VTVlFANJbP"
-const AUTH0_DOMAIN = "amberjohnsonsmile.auth0.com"
-const AUTH0_CALLBACK_URL = location.href
-const AUTH0_API_AUDIENCE = ""
-
 class App extends React.Component {
-  parseHash() {
-    this.auth0 = new auth0.WebAuth({
-      domain: AUTH0_DOMAIN,
-      clientID: AUTH0_CLIENT_ID
-    })
-    this.auth0.parseHash(window.location.hash, (err, authResult) => {
-      if (err) {
-        return console.log(err)
-      }
-      if (
-        authResult !== null &&
-        authResult.accessToken !== null &&
-        authResult.idToken !== null
-      ) {
-        localStorage.setItem("access_token", authResult.accessToken)
-        localStorage.setItem("id_token", authResult.idToken)
-        localStorage.setItem(
-          "profile",
-          JSON.stringify(authResult.idTokenPayload)
-        )
-        window.location = window.location.href.substr(
-          0,
-          window.location.href.indexOf("#")
-        )
-      }
-    })
-  }
-
-  setup() {
-    $.ajaxSetup({
-      beforeSend: (r) => {
-        if (localStorage.getItem("access_token")) {
-          r.setRequestHeader(
-            "Authorization",
-            "Bearer " + localStorage.getItem("access_token")
-          )
-        }
-      }
-    })
-  }
-
-  setState() {
-    let idToken = localStorage.getItem("id_token")
-    if (idToken) {
-      this.loggedIn = true
-    } else {
-      this.loggedIn = false
-    }
-  }
-
-  componentWillMount() {
-    this.setup()
-    this.parseHash()
-    this.setState()
-  }
-
   render() {
-    // if (this.loggedIn) {
-      return <LoggedIn />
-    // }
-    // return <Home />
+    return <Home />
   }
 }
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props)
-    this.authenticate = this.authenticate.bind(this)
-  }
-  authenticate() {
-    this.WebAuth = new auth0.WebAuth({
-      domain: AUTH0_DOMAIN,
-      clientID: AUTH0_CLIENT_ID,
-      scope: "openid profile",
-      audience: AUTH0_API_AUDIENCE,
-      responseType: "token id_token",
-      redirectUri: AUTH0_CALLBACK_URL
-    })
-    this.WebAuth.authorize()
-  }
-
-  render() {
-    return (
-      <div className="container">
-      <div className="row">
-      <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
-      <h1>Jokeish</h1>
-      <p>A load of Dad jokes XD</p>
-      <p>Sign in to get access </p>
-      <a
-      onClick={this.authenticate}
-      className="btn btn-primary btn-lg btn-login btn-block"
-      >
-      Sign In
-      </a>
-      </div>
-      </div>
-      </div>
-    )
-  }
-}
-
-class LoggedIn extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -114,14 +12,6 @@ class LoggedIn extends React.Component {
     }
 
     this.serverRequest = this.serverRequest.bind(this)
-    this.logout = this.logout.bind(this)
-  }
-
-  logout() {
-    localStorage.removeItem("id_token")
-    localStorage.removeItem("access_token")
-    localStorage.removeItem("profile")
-    location.reload()
   }
 
   serverRequest() {
@@ -141,7 +31,6 @@ class LoggedIn extends React.Component {
       <div className="container">
       <br />
       <span className="pull-right">
-      {/* <a onClick={this.logout}>Log out</a> */}
       </span>
       <h2>Jokeish</h2>
       <p>Let's feed you with some funny Jokes!!!</p>
@@ -194,7 +83,7 @@ class Joke extends React.Component {
       </div>
       <div className="panel-body joke-hld">{this.props.joke.joke}</div>
       <div className="panel-footer">
-      {this.props.joke.likes} Likes &nbsp
+      {this.props.joke.likes} Likes &nbsp;
       <a onClick={this.like} className="btn btn-default">
       <span className="glyphicon glyphicon-thumbs-up" />
       </a>
